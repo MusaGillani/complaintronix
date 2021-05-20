76f92520b535028af05df71bb2c5ce59e48376c5
musa <musagillaniwork@gmail.com>
2021-05-20 20:23:07 +0500

    compare hostel head from db via api

--- routes/api/complaints.js
+++ routes/api/complaints.js
@@ -2,38 +2,29 @@ const express = require("express");
 const router = express.Router();
 const db = require("../../models/db");
 
-// const connection = mysqlDb.createConnection({
-//   host: "us-cdbr-east-03.cleardb.com",
-//   user: "b46fe1e6d30bcc",
-//   password: "8cf61177",
-//   database: "heroku_26af7665b162de1",
-// });
-
-// connection.connect((err) => {
-//   if (err) console.log(err);
-//   else {
-//     connection.query("use heroku_26af7665b162de1;", (err) => {
-//       if (err) throw err;
-//     });
-//     console.log("connection successful");
-//   }
-// });
-
 // GET all complaints
 router.get("/", (req, res) => {
-  console.log(req.body);
-  const result = execQuery();
-  res.json(result);
-  res.end();
+  let result;
+  db.query(
+    `SELECT name FROM hostel_heads WHERE name="${req.body.name}" AND email="${req.body.email}";`,
+    function (err, result, fields) {
+      if (err) throw err;
+      else {
+        console.log(result.length);
+        result = result.length != 0 ? true : false;
+        res.send(result);
+      }
+    }
+  );
 });
 
-function execQuery() {
-  let myQuery = `SELECT * FROM hostel_heads;`;
+function execQuery(name, email) {
+  let myQuery = `SELECT name FROM hostel_heads WHERE name="${name}" AND email="${email}";`;
   db.query(myQuery, function (err, result, fields) {
     if (err) throw err;
     else {
-      //   console.log(result);
-      return result;
+      console.log(result.length);
+      return result.length != 0 ? true : false;
     }
   });
 }
