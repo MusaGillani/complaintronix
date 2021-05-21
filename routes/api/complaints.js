20a8a5056b02b704853c5371a117871bc9f2ac9f
musa <musagillaniwork@gmail.com>
2021-05-21 15:03:22 +0500

    added promise in execQuery to avoid callbacks

--- routes/api/complaints.js
+++ routes/api/complaints.js
@@ -4,28 +4,20 @@ const db = require("../../models/db");
 
 // GET all complaints
 router.get("/", (req, res) => {
-  let result;
-  db.query(
-    `SELECT name FROM hostel_heads WHERE name="${req.body.name}" AND email="${req.body.email}";`,
-    function (err, result, fields) {
-      if (err) throw err;
-      else {
-        console.log(result.length);
-        result = result.length != 0 ? true : false;
-        res.send(result);
-      }
-    }
-  );
+  execQuery(req.body.name,req.body.email).then((result) => res.send(result));
 });
 
 function execQuery(name, email) {
-  let myQuery = `SELECT name FROM hostel_heads WHERE name="${name}" AND email="${email}";`;
-  db.query(myQuery, function (err, result, fields) {
-    if (err) throw err;
-    else {
-      console.log(result.length);
-      return result.length != 0 ? true : false;
-    }
+  return new Promise((resolve, reject) => {
+    let myQuery = `SELECT name FROM hostel_heads WHERE name="${name}" AND email="${email}";`;
+    db.query(myQuery, function (err, result, fields) {
+      if (err) reject(err);
+      else {
+        console.log(result.length);
+        // return result.length != 0 ? true : false;
+        resolve(result != 0 ? true : false);
+      }
+    });
   });
 }
 
