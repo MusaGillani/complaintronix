ab4bcbea3d2ffef4405b67e5d647b0363e30dd39
Syed Nabeel <nabeelsharafat@gmail.com>
2022-01-15 19:53:20 +0500

    register student api response

--- models/students.js
+++ models/students.js
@@ -4,36 +4,34 @@ function addStudent(reg_no, name, email, password, hostel_no, room_no) {
   return new Promise((resolve, reject) => {
     let res = 200;
     let myQuery = `SELECT * FROM students WHERE reg_no=${reg_no};`;
-    db.query(myQuery, function (err, result, fields) {
+    db.query(myQuery, (err, result, fields) => {
       if (err) reject(err);
       else {
-        if (result != 0) res = 409;
-        resolve(res);
+        console.log(result);
+        if (result.rows.length == 0) {
+          myQuery = `INSERT INTO students VALUES
+      (${reg_no},'${name}','${email}','${password}',${hostel_no},${room_no});`;
+          db.query(myQuery, function (err, result, fields) {
+            if (err) reject(err);
+            else resolve("added");
+          });
+        } else resolve("exists");
       }
     });
-    // if(res == 200){
-    myQuery = `INSERT INTO students VALUES
-        (${reg_no},'${name}','${email}','${password}',${hostel_no},${room_no});`;
-    db.query(myQuery, function (err, result, fields) {
-      if (err) reject(err);
-      else resolve(res);
-    });
-    // }
   });
 }
 
-
 function checkStudent(email, password) {
-    return new Promise((resolve, reject) => {
-      let myQuery = `SELECT * FROM students WHERE email='${email}' AND password='${password}';`; // query to get check if a hostel_head exists
-      db.query({ text: myQuery, rowMode: "array" }, (err, result) => {
-        if (err) reject(err);
-        else resolve(result.rows.length != 0  ? result : "empty");
-      });
+  return new Promise((resolve, reject) => {
+    let myQuery = `SELECT * FROM students WHERE email='${email}' AND password='${password}';`; // query to get check if a hostel_head exists
+    db.query({ text: myQuery, rowMode: "array" }, (err, result) => {
+      if (err) reject(err);
+      else resolve(result.rows.length != 0 ? result : "empty");
     });
-  }
+  });
+}
 
-  module.exports = {
-    addStudent,
-    checkStudent,
-  };
+module.exports = {
+  addStudent,
+  checkStudent,
+};
--- routes/api/students.js
+++ routes/api/students.js
@@ -7,7 +7,8 @@ const router = express.Router();
 
 // GET if student exists or not
 router.get("/", (req, res) => {
-  students.checkStudent(req.query.email,req.query.password)
+  students
+    .checkStudent(req.query.email, req.query.password)
     .then((result) => res.send(result))
     .catch((error) => {
       logger.error(error.toString());
@@ -17,17 +18,17 @@ router.get("/", (req, res) => {
 
 // ADD a student if not exists
 router.post("/", (req, res) => {
-    const data = req.body;
-    students
-      .addStudent(
-        data.reg_no,
-        data.name,
-        data.email,
-        data.password,
-        data.hostel_no,
-        data.room_no,
-      )
-      .then((result) => res.sendStatus(result));
-  });
+  const data = req.body;
+  students
+    .addStudent(
+      data.reg_no,
+      data.name,
+      data.email,
+      data.password,
+      data.hostel_no,
+      data.room_no
+    )
+    .then((result) => res.send(result));
+});
 
 module.exports = router;
