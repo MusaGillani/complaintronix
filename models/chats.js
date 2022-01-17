1d51d87ab7ca9013d8f0ff6e6eb3d2ce34759f01
musa <musagillaniwork@gmail.com>
2022-01-17 12:26:56 +0500

    using email to and from client instead of reg

--- models/chats.js
+++ models/chats.js
@@ -1,28 +1,46 @@
 const db = require("../config/db");
 
-function addMessage(reg, text) {
+function getRegFromEmail(email) {
   return new Promise((resolve, reject) => {
-    let myQuery = `INSERT INTO chats(msgr_reg,msg_text) VALUES
-    (${reg},'${text}');`;
-    db.query({ text: myQuery }, (err, result) => {
+    let myQuery = `SELECT reg_no FROM students WHERE email='${email}';`;
+    db.query(myQuery, (err, result) => {
       if (err) reject(err);
       else {
-        myQuery = `SELECT * FROM chats;`;
-        db.query(myQuery, (err, result) => {
-          if (err) reject(err);
-          else {
-            // console.log(result.rows);
-            resolve(result.rows);
-          }
-        });
+        resolve(result.rows[0].reg_no);
       }
+    });
+  });
+}
+
+function addMessage(email, text) {
+  return new Promise((resolve, reject) => {
+    let reg;
+    getRegFromEmail(email).then((result) => {
+      reg = result;
+      console.log(reg);
+
+      let myQuery = `INSERT INTO chats(msgr_reg,msg_text) VALUES
+        (${reg},'${text}');`;
+      db.query({ text: myQuery }, (err, result) => {
+        if (err) reject(err);
+        else {
+          myQuery = `SELECT c.msgr_reg, c.msg_text, s.email, c.messaged_at FROM chats c INNER JOIN students s ON c.msgr_reg=s.reg_no;`;
+          db.query(myQuery, (err, result) => {
+            if (err) reject(err);
+            else {
+              // console.log(result.rows);
+              resolve(result.rows);
+            }
+          });
+        }
+      });
     });
   });
 }
 
 function getPreviousMessages() {
   return new Promise((resolve, reject) => {
-    let myQuery = `SELECT * FROM chats;`;
+    let myQuery = `SELECT c.msgr_reg, c.msg_text, s.email, c.messaged_at FROM chats c INNER JOIN students s ON c.msgr_reg=s.reg_no;`;
     db.query(myQuery, (err, result) => {
       if (err) reject(err);
       else {
